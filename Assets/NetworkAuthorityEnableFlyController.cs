bbaaacb1cff5e96f4a05c7fa2fb765aed55e6d37
Alexandre Pia <alex@middlevr.com>
2016-01-30 19:12:40 +0100

    Add head

---
+++ Assets/NetworkAuthorityEnableFlyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkAuthorityEnableFlyController : NetworkBehaviour {
+
+	void Update () {
+        if (!hasAuthority)
+            return;
+
+	    GetComponent<FlyController>().enabled = true;
+
+        enabled = false;
+    }
+}
