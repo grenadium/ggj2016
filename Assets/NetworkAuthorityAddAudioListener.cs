eef284ed8824a41bcb6d7e8f3113410df1abb25e
Alexandre Pia <alex@middlevr.com>
2016-01-30 18:34:20 +0100

    Dynamically add audio Listener to human head

---
+++ Assets/NetworkAuthorityAddAudioListener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkAuthorityAddAudioListener : NetworkBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (!hasAuthority)
+	        return;
+
+	    gameObject.AddComponent<AudioListener>();
+
+	    enabled = false;
+	}
+}
