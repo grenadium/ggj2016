247b07d3577f478c317655ef31527c780e8e6886
Alexandre Pia <alex@middlevr.com>
2016-01-30 21:03:28 +0100

    Integration Tapette

---
+++ Assets/TapetteParenting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TapetteParenting : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    var tapetteRoot = transform.Find("tapette_01/tapette_01_root");
+	    var tapette = transform.Find("tapette_01");
+
+	    tapetteRoot.parent = transform;
+	    tapette.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
