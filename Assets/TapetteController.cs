c48d17dd5c443a40b87f5e3547954fc303fca6ea
Kevin Wagrez <kevin@middlevr.com>
2016-01-31 10:38:23 +0100

    added jingle sounds

--- Assets/TapetteController.cs
+++ Assets/TapetteController.cs
@@ -1,35 +1,111 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TapetteController : NetworkBehaviour {
 
 	// Audio
     public AudioClip[] slapSounds;
+    public AudioClip[] jingleSounds;
+    private AudioSource tapetteAudio;
 
     // Sound characteristics
     private bool slapSoundIsPlaying = false;
     private bool jingleSoundIsPlaying = false;
-    private float audioLength = 0f;
+    private float slapClipLength = 0f;
     private float lastSlapPlayed = 0f;
+    private float jingleFadeDelay = 5f;
+    private float lastJinglePlayed = 0f;
+
+    public float minimumKillSpeed; //Vitesse minimale pour considerer que la tapette tue la mouche
+    public float minimumJamBreakSpeed; //vitesse mini pour casser le pot de confiture
+    public float minimumSoundSpeed; //vitesse mini pour jouer un son de swing
+    private int totalFrames;
+
+    public float currentVelocity;
+    public int nbFramesForVelocity = 5; //nombre de frames à prendre en compte pour le calcul de la vélocité (plus il y a de frames, plus le mouvement doit être grand)
+    private float weightCorrection = 1.2f;
+    private int currentFrame;
+    private List<float> velocities; //velocite de la tapette durant les frames precedentes
+    private Vector3 lastPosition;
 
     #region Monobehaviour callbacks
     void Start ()
     {
+        velocities = new List<float>();
+        currentFrame = 0;
+        lastPosition = transform.position;
+        tapetteAudio = GetComponent<AudioSource>();
         foreach (TapetteCollider collider in GetComponentsInChildren<TapetteCollider>())
             collider.owner = this;
     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (currentFrame < nbFramesForVelocity)
+        {
+            float velocity = ((transform.position - lastPosition).magnitude) / Time.deltaTime; //velocite a la frame actuelle
+            lastPosition = transform.position;
+
+            velocities.Add(velocity);
+            currentFrame++;
+        }
+
+        //quand on atteint la dernière frame prise en compte pour le mouvement
+        else if (currentFrame == nbFramesForVelocity)
+        {
+            float sumVelo = 0;
+            for (int i = 0; i < velocities.Count; i++)
+            {
+                sumVelo += velocities[i];
+            }
+
+            currentVelocity = sumVelo / nbFramesForVelocity;
+
+            velocities.Clear();
+            currentFrame = 0;
+        }
+
+        totalFrames++;
+    }
+
+    void Update ()
+    {
+        if((Time.realtimeSinceStartup - lastJinglePlayed) > jingleFadeDelay)
+        {
+            tapetteAudio.Stop();
+            jingleSoundIsPlaying = false;
+        }
+    }
+
     #endregion
 
     #region Collision events
     public void Slap    (Vector3 position)
     {
-        if((Time.realtimeSinceStartup - lastSlapPlayed) > audioLength)
+        // Slap sounds
+        if((Time.realtimeSinceStartup - lastSlapPlayed) > slapClipLength)
         {
             int index = Random.Range(0, slapSounds.Length - 1);
             AudioSource.PlayClipAtPoint(slapSounds[index], position, 1);
-            audioLength = slapSounds[index].length;
+            slapClipLength = slapSounds[index].length;
             lastSlapPlayed = Time.realtimeSinceStartup;
+        }
+
+        // Jingle
+        if(!jingleSoundIsPlaying)
+        {
+            int index = Random.Range(0, jingleSounds.Length - 1);
+            tapetteAudio.clip = jingleSounds[index];
+            lastJinglePlayed = Time.realtimeSinceStartup;
+            tapetteAudio.Play();
+            jingleSoundIsPlaying = true;
+        }
+        else
+        {
+            lastJinglePlayed = Time.realtimeSinceStartup;
         }
     }
     #endregion
