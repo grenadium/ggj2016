a477f0cf676b9ed690180fd2e45d96d9bac11067
yannismrad <yannismrad@free.fr>
2016-01-31 14:18:09 +0100

    Prefabs images menu (+script) + materials

---
+++ Assets/Sources/Environment/TapetteDisplay.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapetteDisplay : MonoBehaviour {
+    public Sprite flyReadySprite, waitingForFlySprite;
+    public bool switchSprite, hideSprite;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hideSprite)
+            hideDisplay(true);
+
+        else hideDisplay(false);
+	
+	}
+
+    //afficher message fly has arrived
+    public void setDisplayWaitingForFly()
+    {
+        GetComponent<SpriteRenderer>().sprite = waitingForFlySprite;
+    }
+
+    //afficher message fly has arrived
+    public void setDisplayFlyReady()
+    {
+        GetComponent<SpriteRenderer>().sprite = flyReadySprite;
+    }
+
+    public void hideDisplay(bool hide)
+    {
+        if(hide)
+         GetComponent<SpriteRenderer>().enabled = false;
+
+        else
+            GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
---
+++ Assets/Sources/Environment/TimedMenuItem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedMenuItem : MonoBehaviour
+{
+    public float lifeTime;
+    public bool removeMenuItem;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(removeMenuItem)
+        {
+            destroyMenuItem();
+            removeMenuItem = false;
+        }
+    }
+
+    void destroyMenuItem()
+    {
+        iTween.FadeTo(gameObject, 0f, lifeTime);
+        Destroy(gameObject, lifeTime);
+    }
+}
